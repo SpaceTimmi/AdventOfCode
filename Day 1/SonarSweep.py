aabb7b7f8166a095a9c51489678bd6fe04767693
Timi Ombe <timmyombe@gmail.com>
2022-07-28 13:24:27 +0100

    Completed Day 1

--- Day 1/SonarSweep.py
+++ Day 1/SonarSweep.py
@@ -1,14 +1,15 @@
 # Advent Of Code (2021)
 # Day 1: Sonar Scan
 
-
-scanData = open('puzzle.txt', 'r')
+def importData():
+    scanData = open('puzzle.txt', 'r')
+    return scanData
 
 
 # Depth Measurement Increase - Part 1
 def depthMeasurementIncrease(data):
     """given the puzzle data, this function will return the number of
-     times the depth measurement increases for the sonar scan"""
+     times the depth measurement increases for the sonar scan."""
 
     # Total is initialized as -1 because for the first measurement there is no previous measurement.
     # i.e total at first measurement = 0
@@ -22,12 +23,33 @@ def depthMeasurementIncrease(data):
         previousMeasure = newMeasure
     return total
 
-total = depthMeasurementIncrease(scanData)
-print(total)
+depthIncrease = depthMeasurementIncrease(importData())
+print(f"The number of measurements larger than the previous measurements is {depthIncrease}")
 
 
-# Three Measurement Sliding Window - Part 2
+# Three Measurement Sliding Windows - Part 2
 def threeMeasurementSlidingWindow(data):
     """given the puzzle data, this function will return the number of
-     times the sum of three measurements in the slidng window increases"""
-    pass
+    times the sum of three measurements in the slidng window increases."""
+    
+    count = 0
+    newData = []
+    for element in data:
+        newElement = int(element)
+        newData.append(newElement)
+        count += 1
+
+    # if the length of the data is N then there are (N - 2) possbile three measurement sliding windows.
+    possibleWindows = count - 2
+    previousMeasure = 0
+    total = -1
+
+    for index in range(possibleWindows):
+        newMeasure = sum(newData[index:index+3])  
+        if newMeasure > previousMeasure:
+            total += 1
+        previousMeasure = newMeasure
+    return total
+
+threeIncrease = threeMeasurementSlidingWindow(importData())
+print(f"The number of sums larger than the previous sum is {threeIncrease}")
