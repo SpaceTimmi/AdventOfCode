895e8695b14ea364670fc0f12fa9514ab02f34f4
Timi Ombe <timmyombe@gmail.com>
2022-07-29 15:29:02 +0100

    finised the part 2 of Day 2

--- Day 2/Dive.py
+++ Day 2/Dive.py
@@ -5,7 +5,7 @@ def importData():
     scanData = open('input.txt', 'r')
     return scanData
 
-# Final Horizontal Position x Final Depth
+# Final Horizontal Position x Final Depth - Part 1
 def finalPositionAndDepth(data):
     horizontalPosition = 0
     depth = 0
@@ -25,4 +25,30 @@ def finalPositionAndDepth(data):
     return (horizontalPosition * depth)
 
 result = finalPositionAndDepth(importData())
-print(result)
+print(f"The result of multiplying the final horizontal position by the final depth for the input data is {result}")
+
+
+def finalPosAndDepthWithAim(data):
+    horizontalPosition = 0
+    depth = 0
+    aim = 0
+
+    for command in data:
+        newCommand = command.split()
+        move = newCommand[0]
+        change = newCommand[1]
+
+        if move == "down":
+            aim += int(change)
+        elif move == "up":
+            aim -= int(change)
+        elif move == "forward":
+            horizontalPosition += int(change)
+            depth += (aim * int(change)) 
+            pass
+
+    return (horizontalPosition * depth)
+
+
+result2 = finalPosAndDepthWithAim(importData())
+print(f"The result of multiplying the final horizontal postion by the final depth for case 2 is {result2}")
