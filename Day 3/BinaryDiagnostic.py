c2b54de9451082e6e3ff40b2e092fb0ed882e6b7
Timi Ombe <timmyombe@gmail.com>
2022-07-30 14:52:35 +0100

    Backing up part 1 of day 3 (still failing)

---
+++ Day 3/BinaryDiagnostic.py
@@ -0,0 +1,45 @@
+# Advent of Code (2021)
+# Day 3: Binary Diagnostic
+
+def importData():
+    scanData = open('input.txt', 'r')
+    return scanData
+
+# Power consumption of the submarine - Part 1
+def powerConsumption(data):
+    occuranceofZero = [0, 0, 0, 0, 0]
+    occuranceofOne  = [0, 0, 0, 0, 0]
+    
+    for bit in data:
+        newList = list(bit)
+        newList = newList[0:4]  #Removing new line (\n) char at the end
+        
+        for index, element in enumerate(newList):
+            if int(element) == 0:
+                occuranceofZero[index] += 1
+            elif int(element) == 1:
+                occuranceofOne[index] += 1
+
+
+    mostCommon  = [0, 0, 0, 0, 0]
+    leastCommon = [0, 0, 0, 0, 0]
+
+    for i in range(5):
+        if occuranceofZero[i] > occuranceofOne[i]:  
+            mostCommon[i]  = 0
+            leastCommon[i] = 1
+        else:
+            mostCommon[i]  = 1
+            leastCommon[i] = 0
+    
+    mostCommonBinary  = "".join(str(x) for x in mostCommon)
+    leastCommonBinary = "".join(str(y) for y in leastCommon)
+    
+    gammaRate   = int(mostCommonBinary, 2) 
+    epsilonRate = int(leastCommonBinary, 2)
+
+    return gammaRate * epsilonRate
+
+
+result = powerConsumption(importData())
+print(result)
