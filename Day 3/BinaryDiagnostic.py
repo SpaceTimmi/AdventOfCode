75bd78e60f6de3242a344f6eafda3926c6b79e4b
Timi Ombe <timmyombe@gmail.com>
2022-07-31 20:22:27 +0100

    Abstracted the computation of the mostcommon and leastcommon values to its own function so it can be used for solving part 2 of the problem

--- Day 3/BinaryDiagnostic.py
+++ Day 3/BinaryDiagnostic.py
@@ -1,12 +1,23 @@
 # Advent of Code (2021)
 # Day 3: Binary Diagnostic
 
+# Helpers
 def importData():
     scanData = open('input.txt', 'r')
     return scanData
 
-# Power consumption of the submarine - Part 1
-def powerConsumption(data):
+def computeMostAndLeastCommon(data):
+    """
+    Given the data this function returns a tuple of 2 lists.
+    The first list contains the "most" common value for each position i.e.
+    1010 -> 1 is the most common value for position (0), 
+            0 is the most common value for position (1),
+            1 is the most common value for position (2),
+            0 is the most common value for position (3).
+
+    The second list contains the "least" common value for each position (opposite of the first list).
+    0101 -> 0 is the least common value for position (0) etc.
+    """
     occuranceofZero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     occuranceofOne  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
@@ -34,7 +45,18 @@ def powerConsumption(data):
         else:
             mostCommon[i]  = 1
             leastCommon[i] = 0
-    
+
+    return (mostCommon, leastCommon)
+
+
+# Power consumption of the submarine - Part 1
+def powerConsumption(data):
+    """
+    Calculates the power consumption of the submarine.
+    """
+
+    mostCommon, leastCommon = computeMostAndLeastCommon(data)
+
     mostCommonBinary  = "".join(str(x) for x in mostCommon)
     leastCommonBinary = "".join(str(y) for y in leastCommon)
     
@@ -44,5 +66,20 @@ def powerConsumption(data):
     return gammaRate * epsilonRate
 
 
-result = powerConsumption(importData())
-print(result)
+powerC = powerConsumption(importData())
+print(powerC)
+
+
+# Life support rating - Part 2
+def lifeSupportRating(data):
+    """
+    Computes the life support rating of the submarine.
+    """
+
+    mostCommon, leastCommon = computeMostAndLeastCommon(data)
+
+
+
+
+    return 
+    
