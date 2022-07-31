c578c7f1833f8ce9690ff8a20c08778beb73d35f
Timi Ombe <timmyombe@gmail.com>
2022-07-31 21:42:01 +0100

    Failing part 2; currently debugging

--- Day 3/BinaryDiagnostic.py
+++ Day 3/BinaryDiagnostic.py
@@ -2,13 +2,17 @@
 # Day 3: Binary Diagnostic
 
 # Helpers
+
+from lib2to3.pgen2.pgen import generate_grammar
+
+
 def importData():
     scanData = open('input.txt', 'r')
     return scanData
 
 def computeMostAndLeastCommon(data):
     """
-    Given the data this function returns a tuple of 2 lists.
+    Given the data this function returns a tuple of 3 lists.
     The first list contains the "most" common value for each position i.e.
     1010 -> 1 is the most common value for position (0), 
             0 is the most common value for position (1),
@@ -17,16 +21,19 @@ def computeMostAndLeastCommon(data):
 
     The second list contains the "least" common value for each position (opposite of the first list).
     0101 -> 0 is the least common value for position (0) etc.
+
+    The third list is just the input data itself but in a list data structure
     """
     occuranceofZero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     occuranceofOne  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    dataList = list()
     
     for bit in data:
         newList = list(bit)
         
-        # Removing the last element from the list; the last element is the char "\n"
-        end = len(newList) - 1
-        newList = newList[0:end]  
+        end = len(newList) - 1  # Removing the last element from the list; the last element is the char "\n"
+        newList = newList[0:end]
+        dataList.append(newList) # Saving every binary number in a list for future use case. (part -2) 
         
         for index, element in enumerate(newList):
             if int(element) == 0:
@@ -46,16 +53,18 @@ def computeMostAndLeastCommon(data):
             mostCommon[i]  = 1
             leastCommon[i] = 0
 
-    return (mostCommon, leastCommon)
+    return (mostCommon, leastCommon, dataList)
 
 
+# Main
 # Power consumption of the submarine - Part 1
 def powerConsumption(data):
     """
     Calculates the power consumption of the submarine.
     """
 
-    mostCommon, leastCommon = computeMostAndLeastCommon(data)
+    x = computeMostAndLeastCommon(data)
+    mostCommon, leastCommon = x[0], x[1]
 
     mostCommonBinary  = "".join(str(x) for x in mostCommon)
     leastCommonBinary = "".join(str(y) for y in leastCommon)
@@ -74,12 +83,50 @@ print(powerC)
 def lifeSupportRating(data):
     """
     Computes the life support rating of the submarine.
+    Oxgen generator rating and the CO2 scrubber rating
     """
 
-    mostCommon, leastCommon = computeMostAndLeastCommon(data)
+    # Helpers
+    def generateMatch(xs, index, match):
+        """
+        ListOflists, Natural, Natural -> List
+            xs:    list to be considered.
+            index: position in each element of the list to be considered.
+            match: the value xs[index] is compared against
+        returns only valid xs's that have a 'match' at xs[index]
+
+        """
+        result = list()        
+        for elem in xs:
+            if elem[index] == str(match):
+                result.append(elem)
+        return result
+
+    def generateOxyAndCO2(xs, val):
+        """
+        ListOflists, List -> List
+            xs  -> The input data in list data structure
+            val -> Either (mostCommon or leastCommon).
+        This function assumes there will be no ties when comparing values.
+        len(2) and both binary numbers match the val[index]
+        """
+        for index in range(12):
+            result = generateMatch(xs, index, val[index])
+            xs = result
+            if len(xs) == 1:
+                break
+        return xs
+
+    # Main
+    mostCommon, leastCommon, dataList = computeMostAndLeastCommon(data)
+    oxyGenRatingBinary   = generateOxyAndCO2(dataList, mostCommon)
+    scrubberRatingBinary = generateOxyAndCO2(dataList, leastCommon)
+
+    oxyGenRating   = "".join(str(x) for x in oxyGenRatingBinary[0])   
+    scrubberRating = "".join(str(x) for x in scrubberRatingBinary[0])
+
+    return int(oxyGenRating, 2) * int(scrubberRating, 2)
 
 
-
-
-    return 
-    
+lifeSR = lifeSupportRating(importData())
+print(lifeSR)
