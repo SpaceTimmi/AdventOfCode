8fac31cfd4c9c66f88f90d0b2be8509a180cc292
Timi Ombe <timmyombe@gmail.com>
2022-07-31 23:43:21 +0100

    Refactored the determineMostAndLeast function from local to global function

--- Day 3/BinaryDiagnostic.py
+++ Day 3/BinaryDiagnostic.py
@@ -5,51 +5,35 @@
 
 
 def importData():
+    """
+    Takes the input.txt and returns it in a list so it's easy to work with 
+    """
     scanData = open('input.txt', 'r')
+    newList = list()
+    
+    for bit in scanData:
+        tempList = list(bit)
+        end = len(tempList) - 1  # Removing the last element from the list; the last element is the char "\n"
+        tempList = tempList[0:end]
+        newList.append(tempList)
     return scanData
 
-def computeMostAndLeastCommon(data):
-    """
-    Given the data this function returns a tuple of 3 lists.
-    The first list contains the "most" common value for each position i.e.
-    1010 -> 1 is the most common value for position (0), 
-            0 is the most common value for position (1),
-            1 is the most common value for position (2),
-            0 is the most common value for position (3).
-    The second list contains the "least" common value for each position (opposite of the first list).
-    0101 -> 0 is the least common value for position (0) etc.
-    The third list is just the input data itself but in a list data structure
-    """
-    occuranceofZero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    occuranceofOne  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    dataList = list()
-    
-    for bit in data:
-        newList = list(bit)
-        
-        end = len(newList) - 1  # Removing the last element from the list; the last element is the char "\n"
-        newList = newList[0:end]
-        dataList.append(newList) # Saving every binary number in a list for future use case. (part -2) 
-        
-        for index, element in enumerate(newList):
-            if int(element) == 0:
-                occuranceofZero[index] += 1
-            elif int(element) == 1:
-                occuranceofOne[index] += 1
 
+def determineMostandLeast(i, xs):
+    occuranceofZero = 0
+    occuranceofOne  = 0
+    for index, element in enumerate(xs):
+        if int(element[i]) == 0:
+            occuranceofZero += 1
+        elif int(element[i]) == 1:
+            occuranceofOne += 1
 
-    mostCommon  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    leastCommon = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    if (occuranceofZero > occuranceofOne):
+        return (0, 1)
+    else: 
+        return (1, 0)
+   
 
-    for i in range(12):
-        if occuranceofZero[i] > occuranceofOne[i]:  
-            mostCommon[i]  = 0
-            leastCommon[i] = 1
-        else:
-            mostCommon[i]  = 1
-            leastCommon[i] = 0
-
-    return (mostCommon, leastCommon, dataList)
 
 
 # Main
@@ -58,9 +42,17 @@ def powerConsumption(data):
     """
     Calculates the power consumption of the submarine.
     """
-
-    x = computeMostAndLeastCommon(data)
+    
+    """    x = computeMostAndLeastCommon(data)
     mostCommon, leastCommon = x[0], x[1]
+    """
+
+    mostCommon, leastCommon = list(), list()
+
+    for index in range(12):
+        most, least = determineMostandLeast(index, data)
+        mostCommon.append(most)
+        leastCommon.append(least)
 
     mostCommonBinary  = "".join(str(x) for x in mostCommon)
     leastCommonBinary = "".join(str(y) for y in leastCommon)
@@ -106,40 +98,19 @@ def lifeSupportRating(data):
         i.e. a case where xs is of length 2 and both binary numbers match the val[index]
         """
         for index in range(12):
-            match = determineMostandLeastCommon(index, xs)
+            most, least = determineMostandLeast(index, xs)
             if val == 'most':
-                check = match[0]
+                result = generateMatch(xs, index, most)
             else:
-                check = match[1]
-            result = generateMatch(xs, index, check)
+                result = generateMatch(xs, index, least)
             xs = result
             if len(xs) == 1:
                 break
         return xs
-
-    def determineMostandLeastCommon(i, xs):
-        occuranceofZero = 0
-        occuranceofOne  = 0
-        for index, element in enumerate(xs):
-            if int(element[i]) == 0:
-                occuranceofZero += 1
-            elif int(element[i]) == 1:
-                occuranceofOne += 1
-
-        if (occuranceofZero > occuranceofOne):
-            return (0, 1)
-        else: 
-            return (1, 0)
-
+    
     # Main
-    mostCommon, leastCommon, dataList = computeMostAndLeastCommon(data)
-    oxyGenRatingBinary   = generateOxyAndCO2(dataList, 'most')
-    scrubberRatingBinary = generateOxyAndCO2(dataList, 'least')
-
-    #############################################
-    print(f"oxygen generator is {oxyGenRatingBinary}/nwhile most common is {mostCommon}")
-    print(f"scruber rating is {scrubberRatingBinary}/nwhile least common is {leastCommon}") 
-    #############################################
+    oxyGenRatingBinary   = generateOxyAndCO2(data, 'most')
+    scrubberRatingBinary = generateOxyAndCO2(data, 'least')
 
     oxyGenRating   = "".join(str(x) for x in oxyGenRatingBinary[0])   
     scrubberRating = "".join(str(x) for x in scrubberRatingBinary[0])
