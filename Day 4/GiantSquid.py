eda549db8dd53b1ee05ad5724d1514783e7f8ca0
Timi Ombe <timmyombe@gmail.com>
2022-08-04 16:12:41 +0100

    Trying to parse the boards from input.txt

--- Day 4/GiantSquid.py
+++ Day 4/GiantSquid.py
@@ -3,8 +3,15 @@
 
 def importData():
     """
-    Parses the input data into two lists.
-    The first list contains the guesses in sequential order 
+    Parses the input data and returns two lists.
+    The first list contains the guesses in sequential order
+    The second list is a list of boards.
+    list one -> list of numbers 
+                [1,2,3, ....]
+    list two -> list of boards, where board is a list of numbers. i.e.
+                [[1, 2, 3...],
+                [1, 2, 3...],
+                [..........]] 
     """
     scanData = open('input.txt', 'r')
     dataList = list()
@@ -19,17 +26,31 @@ def importData():
     listOfGuesses[-1] = listOfGuesses[-1][:2] # Removing the '\n' char at the end.
     listOfGuesses = [int(elem) for elem in listOfGuesses]
     
-    # ========= Parsing each board (list of numbers) and inputting all boards into a single list =========
+    # ========= Parsing each board out of the data and inputting into a single list =========
     boards = dataList[1::]
     listOfBoards = list()
     for board in boards:
         newBoard = board.split(',')
+        singleBoard = list()
+        
+        for line in newBoard:
+            if line[0] != "\n":
+                temp = line.split(" ")
+                temp[-1] = temp[-1][:-1]
+                temp = [int(elem) for elem in temp if elem]
+                if len(temp) != 0:
+                    singleBoard.append(temp)
+            print(singleBoard) 
+        listOfBoards.append(singleBoard)
+
+
+
         #newBoard[-1] = newBoard[-1][:-1]
 
-        if newBoard[0] != "":
+        #if newBoard[0] != "":
             #newBoard = newBoard[0].split(" ")
             #newBoard = [int(elem) for elem in newBoard if elem]
-            listOfBoards.append(newBoard) 
+            #listOfBoards.append(newBoard) 
     
     print(listOfBoards)
 
